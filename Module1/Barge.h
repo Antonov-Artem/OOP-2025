abaf11413f29b716eb905d7ca3b4e50c70acbee7
Antonov Artem <onlyformywork0@gmail.com>
2025-04-18 07:33:20 +0300

    Reformatted code in Barge.h

--- Module1/Barge.h
+++ Module1/Barge.h
@@ -12,26 +12,24 @@ enum BargeState {
 class Barge {
     const int MAX_MILEAGE = 100; // максимально припустимий пробіг
 
-    int _imo_number;    // унікальний ідентифікатор судна
-    char *_home_port;   // порт приписки
-    char *_ship_owner;  // власник судна
-
     static int cargo_capacity;  // максимальний вантаж (у тоннах)
 
+    int _imo_number;     // унікальний ідентифікатор судна
+    char *_home_port;    // порт приписки
+    char *_ship_owner;   // власник судна
     int _current_cargo;  // поточний вантаж (у тоннах)
     int _mileage;        // пробіг судна (км)
-
-    BargeState _state;  // стан об'єкту "баржа"
+    BargeState _state;   // стан об'єкту "баржа"
 
 public:
     Barge(int imo_number, const char *home_port, const char *ship_owner);
     Barge(const Barge &b, int imo_number);
     ~Barge();
 
+    bool operator>(const Barge &b) const;  // істинна якщо вантаж даної баржі більший ніж вантаж баржі "b"
+
     static int get_cargo_capacity();
     static void set_cargo_capacity(int cargo_capacity);
-
-    bool operator>(const Barge &b) const;  // істинна якщо вантаж даної баржі більший ніж вантаж баржі "b"
 
     void start();                         // почати рух
     void stop();                          // припинити рух
